381646919be6dc75276bb4001fbe35b5c6847466
trisongz <tri@songzcorp.com>
2021-01-05 18:37:33 -0600

    fx

--- src/core/model_handler.py
+++ src/core/model_handler.py
@@ -277,7 +277,7 @@ class ModelHandler(object):
             if mode == 'test' and out_predictions:
                 output.extend(res['predictions'])
                 gold.extend(x_batch['target_src'])
-                src.extend(x_batch['in_graphs'])
+                src.extend(x_batch['targets'])
         if mode == 'test':
             return output, gold, src
         else:
